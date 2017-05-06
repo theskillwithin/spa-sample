08acdb3560878bcc5ea5bde706c42c21278bf23d
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2017-05-06 18:12:33 +0900

    Use process.env.PORT

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const app     = express();
 const path    = require('path');
 const render  = require('./src/server/render');
 
+const port = process.env.PORT || 8080;
+
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.get('/', (req, res, next) => {
     render(req, res);
 });
 
-app.listen(8080, () => {
-    console.log('Listening on port 8080 ...');
+app.listen(port, () => {
+    console.log(`Listening on port ${port} ...`);
 });
