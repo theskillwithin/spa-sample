1b091536e5befce0723cdf73ff7ec966b94467cd
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2017-05-07 13:18:56 +0900

    Tweak

--- src/server/render.js
+++ src/server/render.js
@@ -1,27 +1,23 @@
-const React = require('react');
+const React          = require('react');
 const ReactDOMServer = require('react-dom/server');
-const App = require('./App').default;
+const App            = require('./App').default;
 
 function render(req, res) {
-    const renderContent = ReactDOMServer.renderToString(React.createElement(App));
-    const html = `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8" />
-            <title>Hello SSR</title>
-            <link rel="stylesheet" href="/app.css" type="text/css" media="all" />
-        </head>
-
-
-        <body>
-            <section id="app">
-                ${renderContent}
-            </section>
-            <script type="text/javascript" src="/app.js"></script>
-        </body>
-        </html>
-    `;
+    const content = ReactDOMServer.renderToString(React.createElement(App));
+    const html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title>Hello SSR</title>
+    <link rel="stylesheet" href="/app.css" type="text/css" media="all" />
+</head>
+<body>
+    <section id="app">
+        ${content}
+    </section>
+    <script type="text/javascript" src="/app.js"></script>
+</body>
+</html>`;
 
     res.status(200).send(html);
 };
